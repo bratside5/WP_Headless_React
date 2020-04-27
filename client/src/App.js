c22803b6cf8f1f19c1be7ebc92147cb0b008c161
bratside5 <bratside5@gmail.com>
2020-04-27 10:16:02 +0200

    Added 404 route 😰

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,7 @@ export default App;
 export const NoMatchPage = () => {
   return (
     <div>
-      <h1>404 Error | Page Not Found</h1>
+      <h1>404 Error | Page Not Found...</h1>
     </div>
   );
 };
