0585d530c3de858c7e420113603b07441540c1be
bratside5 <bratside5@gmail.com>
2020-04-29 12:13:14 +0200

    CD Testing

--- client/src/context/PropertyContext.jsx
+++ client/src/context/PropertyContext.jsx
@@ -6,15 +6,13 @@ export const PropertyContext = createContext();
 
 export const PropertyProvider = ({ children }) => {
   const [data, loading] = useFetch("/wp-json/wp/v2/products");
-  if (loading) {
-    return <Loading />;
-  } else {
-    return (
-      <PropertyContext.Provider value={data}>
-        {children}
-      </PropertyContext.Provider>
-    );
-  }
+  // if (loading) {
+  //   return <Loading />;
+  // } else {
+  return (
+    <PropertyContext.Provider value={data}>{children}</PropertyContext.Provider>
+  );
 };
+// };
 
 export default PropertyContext;
